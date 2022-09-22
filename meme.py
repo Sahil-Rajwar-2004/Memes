9e8551b60036f0fedf3d36d883ca69730f678d61
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-09-22 16:05:40 +0530

    Update meme.py

--- meme.py
+++ meme.py
@@ -16,4 +16,9 @@ def index():
     meme_pic,subreddit = get_meme()
     return render_template("meme_index.html",meme_pic = meme_pic,subreddit = subreddit)
 
-app.run(debug = True,host = "0.0.0.0",port = 80)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+app.run(debug = True)
+
